35a84dd959fb840dd71f9563920911bbcdbccbf2
XENA <124408291+snxxsnsn@users.noreply.github.com>
2024-05-20 13:56:33 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,39 +1,29 @@
 function createCalendar(id, year, month) {
     let elem = document.getElementById(id);
-    let mon = month - 1; // 월은 0부터 시작하므로
-    let d = new Date(year, mon);
+    let d = new Date(year, month - 1); // 월은 0부터 시작하므로 1을 빼줍니다.
 
-    let table = '<table><tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr><tr>';
+    // 달력 테이블 헤더에 월 이름을 표시합니다.
+    let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let table = '<table><tr><th colspan="7">' + monthNames[month - 1] + ' ' + year + '</th></tr>';
+
+    table += '<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>';
 
     // 공백 채우기
-    for (let i = 0; i < getDay(d); i++) {
-        table += '<td></td>';
-    }
-
-    // 오늘 날짜 표시
-    let today = new Date();
+    table += '<tr>'.repeat(getDay(d));
 
     // 날짜 채우기
-    while (d.getMonth() == mon) {
-        let cls = '';
-        if (getDay(d) % 7 == 0) cls = 'sunday'; // 일요일
-        if (d.getDate() == today.getDate() && d.getMonth() == today.getMonth() && d.getFullYear() == today.getFullYear()) {
-            cls += ' today'; // 오늘 날짜
-        }
-        table += `<td class="${cls}">` + d.getDate() + '</td>';
-
+    while (d.getMonth() == month - 1) {
+        table += '<td>' + d.getDate() + '</td>';
         if (getDay(d) % 7 == 6) { // 토요일
             table += '</tr><tr>';
         }
-
         d.setDate(d.getDate() + 1);
     }
 
     // 나머지 공백 채우기
     if (getDay(d) != 0) {
-        for (let i = getDay(d); i < 7; i++) {
-            table += '<td></td>';
-        }
+        table += '<td colspan="' + (7 - getDay(d)) + '"></td>';
     }
 
     table += '</tr></table>';
@@ -47,4 +37,10 @@ function getDay(date) { // 일요일부터 0으로 시작
     return day - 1;
 }
 
-createCalendar("calendar", 2024, 5); // 예시로 2024년 5월 달력 생성
+// 현재 날짜를 가져옵니다.
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
+
+// 달력을 생성합니다.
+createCalendar("calendar", currentYear, currentMonth);
