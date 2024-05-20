9192dbd8116e2b0f5fc6b516664edeaf9a8abe1e
XENA <124408291+snxxsnsn@users.noreply.github.com>
2024-05-20 13:47:11 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ function createCalendar(id, year, month) {
     let d = new Date(year, mon);
 
     let table = '<table><tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr><tr>';
+
+    // 공백 채우기
+    for (let i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
     }
 
     // 오늘 날짜 표시
@@ -43,10 +47,4 @@ function getDay(date) { // 일요일부터 0으로 시작
     return day - 1;
 }
 
-// 현재 날짜를 가져옵니다.
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-let currentMonth = currentDate.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
-
-// 달력을 생성합니다.
-createCalendar("calendar", currentYear, currentMonth);
+createCalendar("calendar", 2024, 5); // 예시로 2024년 5월 달력 생성
