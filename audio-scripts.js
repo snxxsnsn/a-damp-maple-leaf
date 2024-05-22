d8fa46926506f2a102440dead1b7b6d3309d41ac
XENA <124408291+snxxsnsn@users.noreply.github.com>
2024-05-22 13:16:51 +0900

    Update audio-scripts.js

--- audio-scripts.js
+++ audio-scripts.js
@@ -12,6 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var songFilename = audioSource.getAttribute("src").split("/").pop(); // 파일 경로에서 파일 이름 추출
     songFilenameElement.textContent = songFilename;
 
+        // 자동 재생이 실패한 경우에 대한 처리
+    if (autoPlayPromise !== undefined) {
+        autoPlayPromise.catch(function(error) {
+            // 자동 재생이 실패한 경우, 재생 버튼을 클릭하여 수동으로 재생할 수 있도록 함
+            playPauseButton.addEventListener("click", function() {
+                audioPlayer.play();
+                playPauseButton.textContent = "❚❚";
+                playPauseButton.removeEventListener("click", arguments.callee); // 클릭 이벤트 리스너 제거
+            });
+        });
+    }
+
     // 현재 시간 업데이트 함수
     function updateCurrentTime(currentTime) {
         currentTimeElement.textContent = formatTime(currentTime);
