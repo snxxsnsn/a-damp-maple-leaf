49b83162c2763b9c88b99bfd3eac971e630f4201
XENA <124408291+snxxsnsn@users.noreply.github.com>
2024-05-26 02:39:01 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ function createCalendar(id, year, month) {
     let mon = month - 1; // 월은 0부터 시작하므로
     let d = new Date(year, mon, 1);
 
+    let header = `<table><tr><th>${year} / ${month}</th></tr>`;
+
     let table = '<table><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr><tr>';
 
     // 공백 채우기
@@ -35,7 +37,10 @@ function createCalendar(id, year, month) {
             table += '<td></td>';
         }
     }
+    
     table += '</tr></table>';
+
+    elem.innerHTML = table;
 }
 
 // 현재 날짜를 기반으로 달력을 생성합니다.
