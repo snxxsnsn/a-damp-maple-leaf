92ba76f3e49cd3f0d9f67dd01ed34da16fe2b0a5
XENA <124408291+snxxsnsn@users.noreply.github.com>
2024-05-26 02:07:28 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function createCalendar(id, year, month) {
     let mon = month - 1; // 월은 0부터 시작하므로
     let d = new Date(year, mon, 1);
 
-    let table = '<table><caption>${year} / ${month}</caption><tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr><tr>';
+    let table = `<table><caption>${year} / ${month}</caption><tr><th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th></tr><tr>`;
 
     // 공백 채우기
     for (let i = 0; i < d.getDay(); i++) {
