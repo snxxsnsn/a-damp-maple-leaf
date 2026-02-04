8e02fea68cff63b1ee7f7716f68749986eb1efa7
XENA <124408291+snxxsnsn@users.noreply.github.com>
2026-02-05 08:37:47 +0900

    Update audio-scripts.js

--- audio-scripts.js
+++ audio-scripts.js
@@ -13,11 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentSongIndex = 0;
     let isShuffle = false;
 
-    const playlist = [
-        { src: 'music/Undertale.mp3', name: 'Undertale OST 090 His Theme' },
-        { src: 'music/LetDown.mp3', name: 'Let Down - Radiohead' },
-        { src: 'music/THORNAPPLE_Galaxy.mp3', name: '은하 - 쏜애플(THORNAPPLE)' }
-    ];
+const playlist = [
+    { src: 'music/LetDown.mp3', name: 'Let Down - Radiohead' }
+];
 
     function loadSong(index) {
         const song = playlist[index];
